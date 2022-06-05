39479001d4a17aa15bcc936a8712571059a8e81f
Serg8787 <tsybulnikserg@gmail.com>
2022-06-05 16:15:16 +0300

    first broadCast

---
+++ app/src/main/java/com/tsybulnik/broadcast/MyReceiver.kt
@@ -0,0 +1,25 @@
+package com.tsybulnik.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        val action = p1?.action
+
+        when(action){
+            Intent.ACTION_AIRPLANE_MODE_CHANGED ->{
+                Log.d("MyReceiver","ACTION_AIRPLANE_MODE_CHANGED")
+                Toast.makeText(p0,"ACTION_AIRPLANE_MODE_CHANGED",Toast.LENGTH_LONG).show()
+            }
+            Intent.ACTION_BATTERY_LOW->{
+                Log.d("MyReceiver","ACTION_AIRPLANE_MODE_CHANGED")
+                Toast.makeText(p0,"ACTION_BATTERY_LOW",Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+}
